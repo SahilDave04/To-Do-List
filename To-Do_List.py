aa648cdd19dd38df126f770d0e109d1a9b71143f
Sahil Dave <sahildave04@gmail.com>
2023-07-04 21:41:02 +0530

    Update To-Do_List.py
    In this update, I have made it more like a widget that stays on your screen and made the UI more minimalistic.

--- To-Do_List.py
+++ To-Do_List.py
@@ -1,62 +1,93 @@
 from PyQt5.QtWidgets import *
-from PyQt5 import QtGui
+from PyQt5 import QtGui, QtCore
+from PyQt5.QtCore import Qt, QPoint
+from PyQt5.QtGui import QStandardItemModel, QStandardItem
+import sys
 from PyQt5 import uic
 import sys
 import mysql.connector as mdb
-
+#from playsound import playsound
 
 class mainwindow(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("TDL.ui", self)
         self.setWindowTitle("To-Do List")
-        #self.setFixedHeight(1010)
-        #self.setFixedWidth(600)
+        self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
+        #self.setWindowFlag(QtCore.Qt.Tool)
+        #self.setWindowOpacity(0.1)
 
+        self.ext = self.findChild(QPushButton,"ext")
+        self.exp = self.findChild(QPushButton,"exp")
         self.task_input = self.findChild(QLineEdit, "task_in")
-        self.due_in = self.findChild(QLineEdit,"due")
-        self.warning = self.findChild(QLabel,"warn")
+        self.phl = self.findChild(QLabel,"first")
         self.add = self.findChild(QPushButton, "add")
-        self.finish = self.findChild(QPushButton, "finish")
-        self.out = self.findChild(QTableWidget,"task_out")
-
-        self.warning.setHidden(True)
-
-        con = mdb.connect(host="localhost", user="root", passwd= "your password", database="database name",auth_plugin='mysql_native_password')
+        self.finish = self.findChild(QPushButton, "sub")
+        self.lst_vw = self.findChild(QListWidget,"task_out")
+        self.st = 0
+        self.task_input.setPlaceholderText("Task")
+        con = mdb.connect(host="localhost", user="root", passwd="drumStick_4011", database="tdl", auth_plugin='mysql_native_password')
         curs = con.cursor()
         q = f"select task,due_in from p_tasks"
         curs.execute(q)
         result = curs.fetchall()
-        print(result)
         if result == []:
-            self.out.horizontalHeader().setVisible(False)
-            self.out.setRowCount(1)
-            self.out.setColumnCount(1)
-            self.out.setItem(0, 0, QTableWidgetItem("No Tasks :)"))
+            self.lst_vw.addItem("No Tasks")
         else:
             rows = len(result)
-            print(rows)
             ele = result[0]
             columns = (len(ele))
-            print(columns)
-            self.out.setRowCount(rows)
-            self.out.setColumnCount(columns)
-            self.out.setHorizontalHeaderLabels(["Task", "Due Date"])
-            self.out.horizontalHeader().setVisible(True)
             for k in range(rows):
-                for l in range(columns):
-                    tup = result[k]
-                    self.out.setItem(k, l, QTableWidgetItem(str(tup[l])))
-                    self.out.setColumnWidth(700, 300)
+                tup = result[k]
+                self.lst_vw.addItem(tup[0])
 
+        self.ext.clicked.connect(self.out)
         self.finish.clicked.connect(self.deletion)
         self.add.clicked.connect(self.adding)
-        self.showMaximized()
+        self.exp.clicked.connect(self.cl)
+        self.phl.setText(self.lst_vw.item(0).text())
+        self.oldPos = self.pos()
+        self.show()
+
+    def out(self):
+        self.close()
+
+    def cl(self):
+        if self.st == 0:
+            width = 480
+            height = 480
+            self.setFixedSize(width,height)
+            self.st = 1
+            self.exp.setText(">")
+            self.phl.setText(self.lst_vw.item(0).text())
+            self.phl.setHidden(True)
+        else:
+            width = 480
+            height = 50
+            self.setFixedSize(width,height)
+            self.st = 0
+            self.exp.setText("<")
+            self.phl.setText(self.lst_vw.item(0).text())
+            self.phl.setHidden(False)
+
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+    def mousePressEvent(self, event):
+        self.oldPos = event.globalPos()
+
+    def mouseMoveEvent(self, event):
+        delta = QPoint (event.globalPos() - self.oldPos)
+        self.move(self.x() + delta.x(), self.y() + delta.y())
+        self.oldPos = event.globalPos()
 
     def deletion(self):
-        m = self.out.currentRow()
-        print("m"+str(m))
-        con = mdb.connect(host="localhost", user="root", passwd= "your password", database="database name",auth_plugin='mysql_native_password')
+        m = self.lst_vw.currentRow()
+    
+        con = mdb.connect(host="localhost", user="root", passwd="drumStick_4011", database="tdl",auth_plugin='mysql_native_password')
         curs = con.cursor()
         q = f"delete from p_tasks where sr_no = '{m+1}'"
         curs.execute(q)
@@ -67,73 +98,44 @@ class mainwindow(QMainWindow):
             q = i
             curs.execute(q)
             con.commit()
+
+        self.lst_vw.clear()
         q = f"select task,due_in from p_tasks"
         curs.execute(q)
         result = curs.fetchall()
-        print(result)
         if result == []:
-            self.out.horizontalHeader().setVisible(False)
-            self.out.clear()
-            self.out.setRowCount(1)
-            self.out.setColumnCount(1)
-            self.out.setItem(0, 0, QTableWidgetItem("No Task :)"))
-
+            self.lst_vw.addItem("No Tasks")
         else:
             rows = len(result)
-            print(rows)
             ele = result[0]
             columns = (len(ele))
-            print(columns)
-            self.out.setRowCount(rows)
-            self.out.setColumnCount(columns)
-            self.out.setHorizontalHeaderLabels(["Task", "Due Date"])
-            self.out.horizontalHeader().setVisible(True)
             for k in range(rows):
-                for l in range(columns):
-                    tup = result[k]
-                    self.out.setItem(k, l, QTableWidgetItem(str(tup[l])))
-                    self.out.setColumnWidth(700, 300)
+                tup = result[k]
+                self.lst_vw.addItem(tup[0])
 
     def adding(self):
         t_inptt = self.task_input.text()
-        d_inptt = self.due_in.text()
         self.task_input.clear()
-        self.due_in.clear()
-        print(t_inptt)
-        if t_inptt != "" and d_inptt == "":
-            self.warning.setText("Enter Due Date !")
-            self.warning.setHidden(False)
-        elif t_inptt == "" and d_inptt != "":
-            self.warning.setText("Enter Task !")
-            self.warning.setHidden(False)
-        elif t_inptt == "" and d_inptt == "":
-            self.warning.setText("Enter Task and Due Date !")
-            self.warning.setHidden(False)
-        else:
-            self.warning.setHidden(True)
-            con = mdb.connect(host="localhost", user="root", passwd= "your password", database="database name",auth_plugin='mysql_native_password')
-            curs = con.cursor()
-            q = f"insert into p_tasks(task,due_in) values('{t_inptt}','{d_inptt}')"
+        con = mdb.connect(host="localhost", user="root", passwd="drumStick_4011", database="tdl",auth_plugin='mysql_native_password')
+        curs = con.cursor()
+        if t_inptt != "":
+            self.task_input.setPlaceholderText("Task")
+            q = f"insert into p_tasks(task,due_in) values('{t_inptt}','0')"
             curs.execute(q)
             con.commit()
+            self.lst_vw.clear()
             q = f"select task,due_in from p_tasks"
             curs.execute(q)
             result = curs.fetchall()
-            print(result)
             rows = len(result)
-            print(rows)
             ele = result[0]
             columns = (len(ele))
-            print(columns)
-            self.out.setRowCount(rows)
-            self.out.setColumnCount(columns)
-            self.out.setHorizontalHeaderLabels(["Task", "Due Date"])
-            self.out.horizontalHeader().setVisible(True)
             for k in range(rows):
-                for l in range(columns):
-                    tup = result[k]
-                    self.out.setItem(k, l, QTableWidgetItem(str(tup[l])))
-                    self.out.setColumnWidth(700, 300)
+                tup = result[k]
+                self.lst_vw.addItem(tup[0])
+
+        else:
+            self.task_input.setPlaceholderText("Invalid")
 
 app = QApplication(sys.argv)
 UIWindow = mainwindow()
